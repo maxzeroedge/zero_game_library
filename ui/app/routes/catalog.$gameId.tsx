41990ee7357a933851796566dd3013d0b4a0e013
Palash <maxzeroedge@gmail.com>
2024-09-14 13:55:48 +0530

    wip

--- ui/app/routes/catalog.$gameId.tsx
+++ ui/app/routes/catalog.$gameId.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { json, useLoaderData } from "@remix-run/react";
 
+const getGameById = async (gameId: string) => {
+    const response = await fetch('http://localhost:8080/game/' + gameId);
+    const data = await response.json();
+    return data.game;
+}
+
 export const loader = async ({ params }: LoaderFunctionArgs) => {
     const gameId = params.gameId;
     const game = await getGameById(gameId);
@@ -8,7 +14,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 };
 
 export const GameCatalogItem = () => {
-    const {game} = useLoaderData();
+    const { game } = useLoaderData();
 
     return (
         <div>{game}</div>
